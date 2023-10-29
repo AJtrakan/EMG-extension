0cfe641b75bd695c1e3dbb894d6cd940ecc4e4b1
trakan tantee <87230169+AJtrakan@users.noreply.github.com>
2023-10-30 01:17:58 +0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -3,7 +3,7 @@
     description: "SG00165 EMG Sensors",
     author: "AJ.TAY",
     category: "Sensors",
-    version: "1.0.0",
+    version: "1.5.0",
     icon: "/static/icon.png", // Category icon
     color: "#0f3058", // Category color (recommend some blocks color)
     blocks: [ // Blocks in Category
